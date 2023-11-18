c603d0d5f73e5e53fa43900c9266df58ec19f0d3
Vishnu dixit <vishnudixit151@gmail.com>
2023-11-18 07:06:26 +0530

    Added in query to search multiple products at a time

--- inventory-service/src/main/java/com/vjsindia/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/vjsindia/inventoryservice/repository/InventoryRepository.java
@@ -13,4 +13,5 @@ import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     Optional<Inventory>  findBySkuCode(String skuCode);
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- inventory-service/src/main/java/com/vjsindia/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/vjsindia/inventoryservice/service/InventoryService.java
@@ -4,6 +4,7 @@ import com.vjsindia.inventoryservice.dto.InventoryResponse;
 import com.vjsindia.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +22,18 @@ public class InventoryService {
     public boolean isInStock(String skuCode) {
         log.info("Checking Inventory");
         return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
 
-
+    @Transactional(readOnly = true)
+    @SneakyThrows
+    public List<InventoryResponse> isProductsInStock(List<String> skuCode) {
+        log.info("Checking Inventory");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
