f40357d78540ef8c1c0d0438ea83c552c021144f
Vishnu dixit <vishnudixit151@gmail.com>
2023-11-18 07:29:53 +0530

    URL added to query multiple items in stock

--- inventory-service/src/main/java/com/vjsindia/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/vjsindia/inventoryservice/controller/InventoryController.java
@@ -1,13 +1,13 @@
 package com.vjsindia.inventoryservice.controller;
 
-import com.vjsindia.inventoryservice.dto.InventoryResponse;
+
 import com.vjsindia.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -20,6 +20,7 @@ public class InventoryController {
     // http://localhost:8082/api/inventory/iphone-13,iphone13-red
 
     // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
+    //http://localhost:1243/api/inventory/checkProducts?skuCode=iphone_13&skuCode=iphone_13_red
     @GetMapping("isInStock/{skuCode}")
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@PathVariable("skuCode") String skuCode) {
